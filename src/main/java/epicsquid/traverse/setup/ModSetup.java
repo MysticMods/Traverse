e89c5986197c17c9f6014297cadf21f671227cad
Jon McManus <due@wxwhatever.com>
2021-01-01 10:01:03 +1000

    Fix values for blocks, fixes #21.

--- src/main/java/epicsquid/traverse/setup/ModSetup.java
+++ src/main/java/epicsquid/traverse/setup/ModSetup.java
@@ -1,7 +1,10 @@
 package epicsquid.traverse.setup;
 
 import epicsquid.traverse.init.ModBlocks;
+import epicsquid.traverse.mixin.AccessorSetFireInfo;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.ComposterBlock;
+import net.minecraft.block.FireBlock;
 import net.minecraft.item.AxeItem;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
@@ -30,6 +33,22 @@ public class ModSetup {
       TileEntityType.SIGN.validBlocks = new HashSet<>(TileEntityType.SIGN.validBlocks);
       TileEntityType.SIGN.validBlocks.add(ModBlocks.FIR_SIGN.get());
       TileEntityType.SIGN.validBlocks.add(ModBlocks.FIR_WALL_SIGN.get());
+
+      AccessorSetFireInfo fire = (AccessorSetFireInfo) ((FireBlock) Blocks.FIRE);
+      fire.invokeSetFireInfo(ModBlocks.FIR_PLANKS.get(), 5, 20);
+      fire.invokeSetFireInfo(ModBlocks.FIR_SLAB.get(), 5, 20);
+      fire.invokeSetFireInfo(ModBlocks.FIR_FENCE_GATE.get(), 5, 20);
+      fire.invokeSetFireInfo(ModBlocks.FIR_FENCE.get(), 5, 20);
+      fire.invokeSetFireInfo(ModBlocks.FIR_STAIRS.get(), 5, 20);
+      fire.invokeSetFireInfo(ModBlocks.FIR_LOG.get(), 5, 5);
+      fire.invokeSetFireInfo(ModBlocks.STRIPPED_FIR_LOG.get(), 5, 5);
+      fire.invokeSetFireInfo(ModBlocks.FIR_WOOD.get(), 5, 5);
+      fire.invokeSetFireInfo(ModBlocks.STRIPPED_FIR_WOOD.get(), 5, 5);
+      fire.invokeSetFireInfo(ModBlocks.FIR_LEAVES.get(), 30, 60);
+      fire.invokeSetFireInfo(ModBlocks.YELLOW_AUTUMNAL_LEAVES.get(), 30, 60);
+      fire.invokeSetFireInfo(ModBlocks.RED_AUTUMNAL_LEAVES.get(), 30, 60);
+      fire.invokeSetFireInfo(ModBlocks.ORANGE_AUTUMNAL_LEAVES.get(), 30, 60);
+      fire.invokeSetFireInfo(ModBlocks.BROWN_AUTUMNAL_LEAVES.get(), 30, 60);
     });
   }
 
