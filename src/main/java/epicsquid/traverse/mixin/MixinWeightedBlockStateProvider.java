296e9e5ea7d244d5f7cb7d7617ba4ab42e77cace
Jon McManus <due@wxwhatever.com>
2021-05-31 10:15:35 +1000

    Fix #43: use TelepathicGrunts's mixin to prevent CME with WeightedBlockStateProvider.

---
+++ src/main/java/epicsquid/traverse/mixin/MixinWeightedBlockStateProvider.java
@@ -0,0 +1,25 @@
+package epicsquid.traverse.mixin;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.world.gen.blockstateprovider.WeightedBlockStateProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+@Mixin(WeightedBlockStateProvider.class)
+public class MixinWeightedBlockStateProvider {
+  private final ReentrantLock lock = new ReentrantLock();
+
+  @Inject(method="getBlockState", at=@At(value="HEAD"))
+  public void lockGetBlockstate (CallbackInfoReturnable<BlockState> cir) {
+    lock.lock();
+  }
+
+  @Inject(method="getBlockState", at=@At(value="RETURN"))
+  public void unlockGetBlockState (CallbackInfoReturnable<BlockState> cir) {
+    lock.unlock();
+  }
+}
